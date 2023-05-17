6a669af13ee483063d60288830a207460ed09372
alekseirechitskiy <slowspirit@gmail.com>
2023-05-17 11:01:06 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9b42e09e-4e9f-4295-b657-28cee2303690';
+export const MODAL_CLOSE_SEC = 2.5;
